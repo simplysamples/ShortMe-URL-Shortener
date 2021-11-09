eef7ac716a9cf9fd37c02fa80ad5636a28c4a394
simplysamples <90924873+simplysamples@users.noreply.github.com>
2021-11-08 20:20:47 -0600

    add redis

--- app/server/db/extensions.py
+++ app/server/db/extensions.py
@@ -1,4 +1,11 @@
 # ------- 3rd party imports -------
 from flask_sqlalchemy import SQLAlchemy
+import redis
+import app.app as a
 
 db = SQLAlchemy()
+redis = redis.Redis(
+  host=a.REDIS_HOST,
+  port=a.REDIS_PORT,
+  password=a.REDIS_PASSWORD
+)
