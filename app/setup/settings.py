57d95231f9c481ccec0c0e2ef1846d9fa46681cd
John <90924873+simplysamples@users.noreply.github.com>
2021-12-01 22:08:52 -0600

    Update database env var

--- app/setup/settings.py
+++ app/setup/settings.py
@@ -1,7 +1,7 @@
 import os
 
 SECRET_KEY = 'mySecretKey' #os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False #os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 ADMIN_USERNAME = 'admin' #os.environ.get('ADMIN_USERNAME')
 ADMIN_PASSWORD = 'admin' #os.environ.get('ADMIN_PASSWORD')
