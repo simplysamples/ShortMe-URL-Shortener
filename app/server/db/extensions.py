754ecf9c302db8d7310894350cd231edede66702
John <90924873+simplysamples@users.noreply.github.com>
2022-02-02 01:03:36 -0600

    Revert "Preview Environment"

--- app/server/db/extensions.py
+++ app/server/db/extensions.py
@@ -1,11 +1,4 @@
 # ------- 3rd party imports -------
 from flask_sqlalchemy import SQLAlchemy
-import redis
-import os
 
 db = SQLAlchemy()
-redis = redis.Redis(
-  host=os.environ.get('REDIS_HOST'),
-  port=os.environ.get('REDIS_PORT'),
-  password=os.environ.get('REDIS_PASSWORD')
-)
